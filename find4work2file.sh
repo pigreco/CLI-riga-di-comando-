0427c38bb9698241b517f17b7a1f21032085f6c4
Salvatore Fiandaca <pigrecoinfinito@gmail.com>
2018-07-31 10:57:07 +0200

    Update find4work2file.sh

--- find4work2file.sh
+++ find4work2file.sh
@@ -1,3 +1,4 @@
+cerca all'interno dei file md il termine'paypal.me' e crea elenco file
 grep -l -r -i --include \*.md  'paypal.me' ./gr_funzioni >./gr_funzioni/da_documentare.txt
 
 questo conta le occorrenze
