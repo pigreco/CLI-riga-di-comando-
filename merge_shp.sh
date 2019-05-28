6e40ddb8d813debb770d75a269e4f4c839d526b9
Salvatore Fiandaca <pigrecoinfinito@gmail.com>
2019-05-28 21:41:54 +0200

    Update merge_shp.sh

--- merge_shp.sh
+++ merge_shp.sh
@@ -9,6 +9,27 @@ for i in $(find . -name '*.shp'); do
         ogr2ogr -f "ESRI Shapefile" -update -append $consolidated_file $i
     fi
 done
-
-
+-------
+# divide i gpkg in shapefile
+#!/bin/bash
+for i in *.gpkg;
+do
+ogr2ogr -f 'ESRI Shapefile' output  "$i"
+done;
+------
+# fonde gli shp in uno solo merged.shp
+#shell OSGeo4W
+for %f in (*.shp) 
+do (
+if not exist merged.shp 
+(ogr2ogr -f “esri shapefile” merged.shp %f) 
+else 
+(ogr2ogr -f “esri shapefile” -update -append merged.shp %f -nln Merged )
+)
+------
+#☺ fonde gli shp in un solo merged.shp
+mapshaper -i *.shp encoding=latin1 combine-files -merge-layers -o merged.shp
+------
+# divide shapefile 
+time mapshaper comuni.shp -split pk_uid -o format=shapefile
 https://www.varunpant.com/posts/merge-multiple-shapefiles-using-ogr2ogr-in-windows-linux-or-osx
