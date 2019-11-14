74d370b8c32f8ed02095f9eb68c8830693301a0c
Salvatore Fiandaca <pigrecoinfinito@gmail.com>
2019-11-14 16:59:03 +0100

    ritaglia raster con gli elementi di uno shapefile

---
+++ ritaglia_con_nome.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -x
+
+# creo due cartelle
+mkdir ./out
+mkdir ./output
+
+# converte lo shapefile reticolo in CSV con il solo attributo nome
+ogr2ogr -f CSV reticolo.csv reticolo.shp -sql "SELECT DISTINCT nome FROM reticolo"
+# crea un file reticolo2 togliendo intestazione
+cat reticolo.csv | tail -n+2 > ./reticolo2.csv
+# ciclo per splittare lo shapefile utilizzando il campo nome
+while read p; do
+	ogr2ogr -f "ESRI Shapefile" -where "nome ='$p'" ./out/$p.shp reticolo.shp;
+done < ./reticolo2.csv;
+# ciclo per ritagliare il raster e assegnare nomefile uguale all'attributo nome
+for i in ./out/*.shp; do
+  filename=$(basename "$i")
+  filename="${filename%.*}"
+# ritaglia il raster
+  gdalwarp -of GTiff -cutline ./out/"$filename".shp -crop_to_cutline -co COMPRESS=LZW ./vrt_2.vrt ./output/"$filename".tif
+done;
+
+# cancello cartella e file inutili
+rm ./out
+rm ./reticolo2.csv
